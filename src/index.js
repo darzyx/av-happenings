3f31e3bfc6849d48a34b349880fd0d4bafad242a
Jose Dario Sanchez <josedario.xyz@gmail.com>
2018-03-05 23:12:34 -0800

    Adds Redux dependencies/boilerplate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import rootReducer from './reducers/index'
 import 'semantic-ui-css/semantic.min.css'
 import './styles/index.css'
 import App from './App'
 import registerServiceWorker from './utilities/registerServiceWorker'
 
+const middleware = [thunk]
+if (process.env.NODE_ENV !== 'production') { middleware.push(createLogger()) }
+
+const store = createStore(rootReducer, applyMiddleware(...middleware))
+
 const rootDOMNode = document.getElementById('root')
 
-render(<App />, rootDOMNode)
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootDOMNode
+)
 
 registerServiceWorker()

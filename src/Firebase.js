7e3c19f416bdda928d4de7217db93cf5b4a019be
Jose Dario Sanchez <josedario.xyz@gmail.com>
2018-03-05 23:05:13 -0800

    configures Firebase

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDHjXI07A9Jk0NobMnif3GZzCmG0OpK75Y",
+  authDomain: "my-demo-project-20371.firebaseapp.com",
+  databaseURL: "https://my-demo-project-20371.firebaseio.com",
+  projectId: "my-demo-project-20371",
+  storageBucket: "my-demo-project-20371.appspot.com",
+  messagingSenderId: "734902640250"
+}
+
+firebase.initializeApp(FIREBASE_CONFIG)
+
+export const eventsDB = firebase.database().ref('events')
